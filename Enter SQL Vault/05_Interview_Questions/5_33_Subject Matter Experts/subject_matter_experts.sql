a23bf361b8423ad456bca305427e242a717ba717
pavlosemeniuk <paulsemenyuk@gmail.com>
2025-01-20 10:11:07 -0700

    5.33 Solution

--- Enter SQL Vault/05_Interview_Questions/5_33_Subject Matter Experts/subject_matter_experts.sql
+++ Enter SQL Vault/05_Interview_Questions/5_33_Subject Matter Experts/subject_matter_experts.sql
@@ -7,3 +7,13 @@ group by employee_id
 HAVING sum(years_of_experience) >= 8 and count(domain)=1
   or count(domain)=2 and sum(years_of_experience) >= 12
 ;
+
+
+-- Self Join
+
+SELECT DISTINCT a.employee_id
+FROM employee_expertise a
+JOIN employee_expertise b ON a.employee_id = b.employee_id  /* check every pair of skills per employee */
+WHERE
+  ((a.domain = b.domain)  AND (a.years_of_experience >= 8)) OR   /* 8 years in 1 domain? */
+  ((a.domain != b.domain) AND (a.years_of_experience + b.years_of_experience >= 12))  /* or 12 years in 2 domains? */
