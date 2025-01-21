8cb353c99598f2c81c1850cb7986e0c9d2c21d0f
pavlosemeniuk <paulsemenyuk@gmail.com>
2025-01-21 07:39:25 -0700

    5.34 Solution

--- Enter SQL Vault/05_Interview_Questions/5_34_LinkedIn Power Creators/linkedIn_power_creators.sql
+++ Enter SQL Vault/05_Interview_Questions/5_34_LinkedIn Power Creators/linkedIn_power_creators.sql
@@ -1,4 +1,15 @@
--- Solution
+-- Solution P1 (Easy)
+
+SELECT
+  pp.profile_id
+FROM personal_profiles as pp
+JOIN company_pages as cp
+  ON pp.employer_id=cp.company_id
+WHERE
+pp.followers>cp.followers
+ORDER BY pp.profile_id;
+
+-- Solution P2 (Medium) - dif
 
 SELECT
   pp.profile_id
